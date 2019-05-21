93b3669037dcf469ce861b718116692c139d7088
baron <owenashby@hotmail.nl>
2019-05-21 16:50:51 +0200

    initial code commit

---
+++ BooksAPITest/BooksAPITest/BookSearch.cs
@@ -0,0 +1,110 @@
+﻿using Google.Apis.Auth.OAuth2;
+using Google.Apis.Books.v1;
+using Google.Apis.Books.v1.Data;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BooksAPITest
+{
+    class BookSearch
+    {
+        private readonly string configFile = "C:/Users/owena/git/Books API Test/BooksAPITest/BooksAPITest/config.txt";
+        public static BooksService service = new BooksService(new BaseClientService.Initializer
+        {
+        });
+
+        public async Task CreateServiceObject()
+        {
+            Dictionary<string, string> config = ReadConfigFile(configFile);
+
+            UserCredential credential;
+            credential = await GoogleWebAuthorizationBroker.AuthorizeAsync(
+                new ClientSecrets
+                {
+                    ClientId = config["client_id"],
+                    ClientSecret = config["client_secret"]
+                },
+                new[] { BooksService.Scope.Books },
+                "user",
+                CancellationToken.None,
+                new FileDataStore("Books.ListMyLibrary"));
+
+            service = new BooksService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApiKey = config["api_key"],
+                ApplicationName = "AR Books"
+            });
+        }
+
+        private Dictionary<string, string> ReadConfigFile(string filePath)
+        {
+            Dictionary<string, string> configDict = new Dictionary<string, string>();
+            foreach (string line in File.ReadLines(filePath, Encoding.UTF8))
+            {
+                string[] words = line.Split(' ');
+                words[0] = words[0].Replace(":", string.Empty);
+                configDict.Add(words[0], words[1]);
+            }
+            return configDict;
+        }
+
+        public static async Task<Volume> SearchISBN(string isbn)
+        {
+            Console.WriteLine("Executing a book search request for ISBN: {0} ...", isbn);
+            // Call API
+            var result = await service.Volumes.List(isbn).ExecuteAsync();
+
+            if (result != null && result.Items != null)
+            {
+                var item = result.Items.FirstOrDefault();
+                return item;
+            }
+            return null;
+        }
+
+        public static async Task<Bookshelf> ListMyFirstShelf(string userId)
+        {
+            Console.WriteLine("Listing the first bookshelf of user with ID {0}...", userId);
+            // Call API
+            var result = await service.Mylibrary.Bookshelves.List().ExecuteAsync();
+            if (result != null && result.Items != null)
+            {
+                var item = result.Items.First();
+                return item;
+            }
+            return null;
+        }
+
+        public static async Task<Bookshelves> ListAllMyShelves(string userId)
+        {
+            Console.WriteLine("Listing all bookshelves of user with ID {0}...", userId);
+            // Call API
+            var result = await service.Mylibrary.Bookshelves.List().ExecuteAsync();
+            if (result != null && result.Items != null)
+            {
+                return result;
+            }
+            return null;
+        }
+
+        public static async Task<Volumes> ListVolumesOnShelf(string shelfId)
+        {
+            Console.WriteLine("Listing volumes on shelf with ID {0}...", shelfId);
+            // Call API
+            var result = await service.Mylibrary.Bookshelves.Volumes.List(shelfId).ExecuteAsync();
+            if (result != null && result.Items != null)
+            {
+                return result;
+            }
+            return null;
+        }
+    }
+}
---
+++ BooksAPITest/BooksAPITest/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BooksAPITest
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                new BookSearch().CreateServiceObject().Wait();
+                TestApi();
+            }
+            catch (AggregateException ex)
+            {
+                foreach (var e in ex.InnerExceptions)
+                {
+                    Console.WriteLine("ERROR {0}", e.Message);
+                }
+            }
+        }
+
+        private static void TestApi()
+        {
+            string isbn = "0071807993";
+            string userId = "105479000489381502269";
+            string shelfId = "1002";
+
+            Console.WriteLine(BookSearch.service.ApplicationName);
+
+            Console.WriteLine("\n------------------------------------------------------------------------");
+
+            var output = BookSearch.SearchISBN(isbn);
+            var result = output.Result;
+
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine("Book Name: \t" + result.VolumeInfo.Title);
+            Console.WriteLine("Author: \t" + result.VolumeInfo.Authors.FirstOrDefault());
+            Console.WriteLine("Publisher: \t" + result.VolumeInfo.Publisher);
+            Console.WriteLine("------------------------------------------------------------------------\n");
+
+            var shelf = BookSearch.ListMyFirstShelf(userId);
+            var rest = shelf.Result;
+
+            Console.WriteLine("------------------------------------------------------------------------");
+            Console.WriteLine("Shelf Name: \t {0}", rest.Title);
+            Console.WriteLine("Access: \t {0}", rest.Access);
+            Console.WriteLine("------------------------------------------------------------------------\n");
+
+            var shelves = BookSearch.ListAllMyShelves(userId);
+            var s = shelves.Result.Items;
+            Console.WriteLine("------------------------------------------------------------------------");
+            foreach (var bookshelf in s)
+            {
+                Console.WriteLine("Shelf name: \t{0}", bookshelf.Title);
+                Console.WriteLine("Access type: \t{0}", bookshelf.Access);
+                Console.WriteLine("Shelf ID: \t{0}\n", bookshelf.Id);
+            }
+            Console.WriteLine("------------------------------------------------------------------------\n");
+
+            var booksOnShelf = BookSearch.ListVolumesOnShelf(shelfId);
+            var books = booksOnShelf.Result.Items;
+            Console.WriteLine("------------------------------------------------------------------------");
+            foreach (var book in books)
+            {
+                Console.WriteLine("Book name: \t{0}", book.VolumeInfo.Title);
+                Console.WriteLine("Book ID: \t{0}", book.Id);
+                foreach (var author in book.VolumeInfo.Authors)
+                {
+                    Console.WriteLine("\tAuthor: \t{0}", author);
+                }
+                Console.WriteLine("{0}", book.AccessInfo.Embeddable);
+            }
+            Console.WriteLine("------------------------------------------------------------------------");
+        }
+    }
+}
